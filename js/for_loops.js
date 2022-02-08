240b8fad86b18c31dbea028c5a2edaf92b4b9304
AlfredoGaitan <gaitan.alfredo78@gmail.com>
2022-02-07 19:48:14 -0600

    added while and for loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,29 @@
+// Ex. #5
+// for(var count = 100; count >=5; count = count -5){
+//     console.log(count);
+// }
+
+//Ex. #2
+// var showMultiplicationTable = 7;
+// for (var i = 1; i <=10; i++){
+//     var result = showMultiplicationTable * i;
+//     console.log(showMultiplicationTable + ' * ' + i + ' = ' + result);
+// }
+
+//Ex. #3
+// var number = Math.floor(Math.random() *2) +1;
+// for(var i = 20; i <=200; i++){
+//     if(i % 2 === 0){
+//         console.log(i + ' is even');
+//     }else{
+//         console.log(i + ' is odd');
+//     }
+// }
+
+//Ex #4
+
+
+
+
+
+
